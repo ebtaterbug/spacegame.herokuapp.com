0e4659b7fd618f14c30a2f5ec5d91e7ee54b567f
ebtaterbug <ityjoseph@gmail.com>
2022-12-18 09:38:18 -0800

    added boundaries and names

--- server.js
+++ server.js
@@ -13,17 +13,16 @@ app.get('/', (req, res) => {
 
 const FPS = 100
 const SHIP_SIZE = 25
-const TURN_SPEED = 270
 const SHIP_THRUST = 8
 
 players = new Map()
 bullets = new Map()
 
 var moon = {
-    mass: 10,
+    mass: 3000,
     x: 0,
     y: 0,
-    r: 100
+    r: 2000
 }
 
 class Bullet {
@@ -45,38 +44,17 @@ class Bullet {
     this.y += this.thrust.y
   }
 
-  attraction() {  
-    const sx = this.x;
-    const sy = this.y;
-    const ox = moon.x;
-    const oy = moon.y;
-    const dx = ox-sx;
-    const dy = oy-sy;
-    const d = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
-
-    if (d==0) return [0, 0];
-
-    const F = 10000 * this.mass * moon.mass / Math.pow(d, 2);
-
-    const o = Math.atan2(dy, dx);
-    const Fx = Math.cos(o) * F;
-    const Fy = Math.sin(o) * F;
-    
-    this.thrust.x += Fx / FPS
-    this.thrust.y += Fy / FPS
-  }
-
   collision(object) {
       let squareDistance = (this.x-object.x)*(this.x-object.x) + (this.y-object.y)*(this.y-object.y);
-      return squareDistance <= ((this.r + object.r) * (this.r + object.r))
+      return squareDistance <= ((this.r + (object.r+5)) * (this.r + (object.r+5)))
   }
 
 }
 
 class Ship {
   constructor() {
-      this.x = 100;
-      this.y = 100;
+      this.x = -2000;
+      this.y = -2000;
       this.r = SHIP_SIZE / 2;
       this.a = 315 / 180 * Math.PI;
       this.rot = 0;
@@ -86,7 +64,8 @@ class Ship {
           y: 0
       };
       this.mass = 1;
-      this.id
+      this.id;
+      this.name;
   }
 
   rotate(){
@@ -140,11 +119,15 @@ io.on("connection", (socket) => {
     players.delete(socket.id)
   })
 
+  socket.on("submit", (name) => {
+    players.get(socket.id).name = name
+  })
+
   socket.on("shoot", () => {
     player = players.get(socket.id)
     bullet = new Bullet(player.x, player.y)
-    bullet.thrust.x += 1800 * Math.cos(player.a) / FPS
-    bullet.thrust.y -= 1800 * Math.sin(player.a) / FPS
+    bullet.thrust.x += (50+player.thrust.x) * Math.cos(player.a)
+    bullet.thrust.y -= (50-player.thrust.y) * Math.sin(player.a)
     bullet.id = socket.id
     bullets.set(bullet.x, bullet)
   })
@@ -156,44 +139,12 @@ io.on("connection", (socket) => {
     )+309.43
   })
 
-  socket.on("leftKeyDown", () => {
-    players.get(socket.id).rot = TURN_SPEED / 180 * Math.PI / FPS
-  })
-
-  socket.on("rightKeyDown", () => {
-    players.get(socket.id).rot = -TURN_SPEED / 180 * Math.PI / FPS
-  })
-
-  socket.on("left", () => {
-    players.get(socket.id).rot += TURN_SPEED / 180 * Math.PI / FPS
-  })
-
-  socket.on("right", () => {
-    players.get(socket.id).rot += -TURN_SPEED / 180 * Math.PI / FPS
-  })
-
   socket.on("upKeyDown", () => {
     players.get(socket.id).thrusting = true
-   })
-
-  socket.on("leftKeyUp", () => {
-    players.get(socket.id).rot = 0
-  })
-
-  socket.on("rightKeyUp", () => {
-    players.get(socket.id).rot = 0
   })
 
   socket.on("upKeyUp", () => {
     players.get(socket.id).thrusting = false
-  })
-
-  socket.on("thrust", () => {
-    if (players.get(socket.id).thrusting == true) {
-      players.get(socket.id).thrusting = false
-    } else {
-      players.get(socket.id).thrusting = true
-    }
   })
 
   setInterval(emit, 2000 / FPS)
@@ -213,21 +164,20 @@ function update() {
         if (ship.collision(moon)) {
             ship.thrust.x = -ship.thrust.x/2
             ship.thrust.y = -ship.thrust.y/2
-            // if ((ship.thrust.x > 1 || ship.thrust.y > 1)) {
-            //   ship.thrust.x = 0
-            //   ship.thrust.y = 0
-            //   ship.x = 500
-            //   ship.y = 500
-            // }
+        }
+        if ((ship.x < -5000) || (ship.x > 5000)) {
+          ship.thrust.x = 0
+        }
+        if ((ship.y < -5000) || (ship.y > 5000)) {
+          ship.thrust.y = 0
         }
     }
     
-    for (const [id, bullet]  of bullets) {
+    for (const [id, bullet] of bullets) {
       if (bullet.timer < 1) {
         bullets.delete(id)
       }
       bullet.move()
-      bullet.attraction()
       if (bullet.collision(moon)) {
         bullet.thrust.x = -bullet.thrust.x/2
         bullet.thrust.y = -bullet.thrust.y/2
@@ -236,8 +186,8 @@ function update() {
         if (bullet.collision(ship) && (ship.id != bullet.id)) {
           ship.thrust.x = 0
           ship.thrust.y = 0
-          ship.x = 500
-          ship.y = 500
+          ship.x = -2000
+          ship.y = -2000
         }
       }
       bullet.timer -= 1
